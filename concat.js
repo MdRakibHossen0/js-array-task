345c80142f14d39e0a71c4e03cab5453915d7b30
MdRakibHossen0 <mdrakibhossencse@gmail.com>
2025-02-08 23:25:02 +0600

    added concat and isArray

---
+++ concat.js
@@ -0,0 +1,14 @@
+/**
+ * Combining Arrays
+Instructions:
+
+*Create two arrays of your choice.
+*Use the concat method to combine the two arrays into a new array.
+*Print both the original arrays and the combined array using console.log().
+ */
+
+const array1 = ['apple', 'ball', 'cat'];
+const array2 = ['250', '50', '2200'];
+const array3 = array1.concat(array2);
+
+console.log(array3);
---
+++ isArray.js
@@ -0,0 +1,23 @@
+/** Checking if it's an Array
+Instructions:
+
+*Create different variables, each containing either an array or a non-array value.
+
+*Now use isArray to check if each variable is an array.
+
+*Print a message to the console indicating whether each variable is an array or not.
+ 
+ */
+
+let letter =  ["a","e","b","g","p","0"];
+
+let nums = [];
+
+let food = 'ros o g o l a'
+
+console.log(Array.isArray(letter))
+
+
+
+
+
